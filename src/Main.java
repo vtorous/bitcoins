af5da2c198c9ce8240889e6a17db3ae34317ba6b
Vasyl Torous <vtorous@gmail.com>
2022-09-14 00:50:52 +0300

    some commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         float bitcoinsPrice;    // Current bitcoin price
         float usdAmount;        // Amount of dollars
         float btcAmount;        // Amount of bitcoins
